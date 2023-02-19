15b85e642f91bea558ed00c8a9d7b76cd58f3e05
michele <mick0974@outlook.it>
2023-02-19 16:34:47 +0100

    added DesktopChanger

---
+++ DesktopChanger/DesktopChanger/Program.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Win32;
+using System.Runtime.InteropServices;
+
+namespace DesktopChanger
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            File.WriteAllBytes("tanuki_the_dropper.exe", DesktopChanger.Properties.Resources.tanuki_the_dropper);
+            Wallpaper.Set(Wallpaper.Style.Centered);
+        }
+    }
+
+    public static class Wallpaper
+    {
+        const int SPI_SETDESKWALLPAPER = 20;
+        const int SPIF_UPDATEINIFILE = 0x01;
+        const int SPIF_SENDWININICHANGE = 0x02;
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+
+        public enum Style : int
+        {
+            Tiled,
+            Centered,
+            Stretched
+        }
+
+        public static void Set(Style style)
+        {
+            File.WriteAllBytes("tanuki_wallpaper.bmp", DesktopChanger.Properties.Resources.tanuki);
+            string tempPath = "tanuki_wallpaper.bmp";
+
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Control Panel\Desktop", true);
+            if (style == Style.Stretched)
+            {
+                key.SetValue(@"WallpaperStyle", 2.ToString());
+                key.SetValue(@"TileWallpaper", 0.ToString());
+            }
+            if (style == Style.Tiled)
+            {
+                key.SetValue(@"WallpaperStyle", 0.ToString());
+                key.SetValue(@"TileWallpaper", 1.ToString());
+            }
+            if (style == Style.Centered)
+            {
+                key.SetValue(@"WallpaperStyle", 0.ToString());
+                key.SetValue(@"TileWallpaper", 0.ToString());
+            }
+
+            SystemParametersInfo(SPI_SETDESKWALLPAPER,
+                0,
+                tempPath,
+                SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
+        }
+    }
+}
